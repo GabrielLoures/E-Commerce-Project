1e87817dab70395f2dba7b972491dcc069e778f2
Gabriel <gabrielloures21@gmail.com>
2023-01-27 10:02:01 -0300

    End Testimonials Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Products from "./components/Products/Products";
 import Slider from "./components/Slider/Slider";
+import Testimonials from "./components/Testimonials/Testimonials";
 import Virtual from "./components/Virtual/Virtual";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Slider />
       <Virtual />
       <Products />
+      <Testimonials />
     </div>
   );
 }
---
+++ src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import css from './Testimonials.module.css'
+
+import Hero from '../../assets/testimonialHero.png'
+import { TestimonialsData } from '../../data/testimonials'
+
+const Testimonials = () => {
+  return (
+    <div className={css.testimonials}>
+
+      <div className={css.wrapper}>
+        <div className={css.container}>
+          <span>Mais Vendidos</span>
+          <span>Os melhores produtos de beleza em um só lugar. Confira os depoimentos de nossos clientes</span>
+        </div>
+
+        <img src={Hero} alt="Imagem de uma mulher" />
+
+        <div className={css.container}>
+          <span>100k</span>
+          <span>Clientes satisfeitos</span>
+        </div>
+      </div>
+
+    <div className={css.reviews}>Depoimentos</div>
+
+      <div className={css.carousel}>
+        <Swiper
+          slidesPerView={3}
+          slidesPerGroup={1}
+          spaceBetween={20}
+          className={css.tCarousel}
+        >
+          {
+            TestimonialsData.map((testimonial, index) =>(
+              <SwiperSlide>
+                <div className={css.testimonial}>
+                  <img src={testimonial.image} alt="Foto do autor do depoimento" />
+                  <span>{testimonial.comment}</span>
+                  <hr />
+                  <span>{testimonial.name}</span>
+                </div>
+              </SwiperSlide>
+            ))
+          }
+        </Swiper>
+      </div>
+
+    </div>
+  )
+}
+
+export default Testimonials
