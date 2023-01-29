6b6c16746ce1033db195640a7f659ccf17bad87c
Gabriel <gabrielloures21@gmail.com>
2023-01-29 10:13:50 -0300

    End Tablet Responsive Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import { useState } from 'react'
 import css from './Header.module.css'
 
 import Logo from '../../assets/logo.png'
 
 import { CgShoppingBag } from 'react-icons/cg'
+import { GoThreeBars } from 'react-icons/go'
 
 const Header = () => {
+
+  const [showMenu, setShowMenu] = useState(true)
+
+  const toggleMenu = () => {
+    setShowMenu((showMenu) => !showMenu)
+  }
+
   return (
     <div className={css.container}>
 
@@ -15,16 +24,21 @@ const Header = () => {
       </div>
 
       <div className={css.right}>
-        <div className={css.menu}>
-          <ul className={css.menu}>
+
+        <div className={css.sandwichMenu} onClick={toggleMenu}>
+          <GoThreeBars />
+        </div>
+
+        {/* No style dizemos que, se o showMenu for true, o display será igual do elemento pai (flex no caso), se for falso o display será none */}
+        
+          <ul className={css.menu}  style={{display: showMenu ? 'inherit' : 'none'}}>
             <li>Coleções</li>
             <li>Marcas</li>
             <li>Lançamentos</li>
             <li>Promoções</li>
             <li>PT-BR</li>
           </ul>
-        </div>
-
+        
         <input type="text" className={css.search} placeholder="Buscar"/>
 
         <CgShoppingBag className={css.cart} />
